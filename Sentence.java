78404d16d7cc2f86ee7b36ffe33911d5f89f3697
yoonjung <choiyj35@gmail.com>
2013-08-16 11:35:31 -0400

    add SentenceSplitter.java

---
+++ Sentence.java
@@ -0,0 +1,12 @@
+
+public class Sentence {
+		public int beg;
+		public int end;
+		public String sent;
+		
+		public Sentence(String sent, int beg, int end){
+			this.sent = sent;
+			this.beg = beg;
+			this.end = end;
+		}
+	}
---
+++ SentenceSplitter.java
@@ -0,0 +1,39 @@
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.stanford.nlp.ling.HasWord;
+import edu.stanford.nlp.ling.TaggedWord;
+import edu.stanford.nlp.tagger.maxent.MaxentTagger;
+
+
+public class SentenceSplitter {
+
+	private MaxentTagger tagger;
+	
+	public SentenceSplitter(String path){
+		try {
+			this.tagger = new MaxentTagger(path);
+		} catch (ClassNotFoundException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("Can not load stanford model!");
+		}
+	}
+	
+	public List<Sentence> process(String text){
+		StringReader sReader = new StringReader(text);
+		List<Sentence> sents = new ArrayList<Sentence>();
+		
+		List<List<HasWord>> sentences = MaxentTagger.tokenizeText(sReader);
+		for (List<HasWord> sentence : sentences) {
+			List<TaggedWord> tSentence = tagger.tagSentence(sentence);
+			sents.add(new Sentence(text.substring(tSentence.get(0).beginPosition(), tSentence.get(tSentence.size()-1).endPosition()),
+					               tSentence.get(0).beginPosition(), tSentence.get(tSentence.size()-1).endPosition()));
+		}
+
+		return sents;
+	}
+	
+}
