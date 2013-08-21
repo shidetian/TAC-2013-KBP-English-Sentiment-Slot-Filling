d0f5c2724788e1cc223263bb96bb1892e1304698
LingjiaDeng <lingjiadeng@gmail.com>
2013-08-20 21:12:03 -0400

    representing an extracted opinion from sentiment analysis system

---
+++ SentimentUnit.java
@@ -0,0 +1,24 @@
+// each opinion extracted from any sentiment analysis system 
+// is represented by this class
+
+public class SentimentUnit{
+	
+	// we can add more public attributes, if needed
+	public String sentenceSpan;  // span of the sentence containing this opinion
+	public String opinSpan;  // span of the opinion expression
+	public String holderSpan;  // span of the holder
+	public String targetSpan;  // span of the target
+	public String polarity;    // polarity, pos or neg
+	public double confidenceScore;  // confidence score of the extracted opinion
+	
+	public SentimentUnit(String sentenceSpan, String opinSpan, String holderSpan, 
+			String targetSpan, String polarity, double confidenceScore){
+		this.sentenceSpan = sentenceSpan;
+		this.opinSpan = opinSpan;
+		this.holderSpan = holderSpan;
+		this.targetSpan = targetSpan;
+		this.polarity = polarity;
+		this.confidenceScore = confidenceScore; 
+	}
+	
+}
