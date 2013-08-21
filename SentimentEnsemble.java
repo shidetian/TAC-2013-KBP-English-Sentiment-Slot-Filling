502801199cca0bca7c50d1a512d7886fa9c71331
LingjiaDeng <lingjiadeng@gmail.com>
2013-08-20 23:56:29 -0400

    Update SentimentEnsemble.java

--- SentimentEnsemble.java
+++ SentimentEnsemble.java
@@ -3,8 +3,6 @@
 // For any two sentiment units sharing the same holder and target, 
 // the output will be only one
 
-// Finally it will generate a list of non-duplicate sentiment units
-
 import java.util.ArrayList;
 
 public class SentimentEnsemble{
@@ -12,21 +10,14 @@ public class SentimentEnsemble{
 	public ArrayList<SentimentUnit> sentimentList;
 	public ArrayList<SentimentUnit> sentimentListEnsembled;
 	
-	public SentimentEnsemble(ArrayList<SentimentUnit> sentimentList){
-		this.sentimentList = sentimentList;
+	public SentimentEnsemble(){
+		this.sentimentList = new ArrayList<SentimentUnit>();
 		this.sentimentListEnsembled = new ArrayList<SentimentUnit>();
-	}
-	
-	// add one more sentiment unit
-	public void addSentimentUnit(SentimentUnit su){
-		this.sentimentList.add(su);
-		return;
 	}
 	
 	// add a list of sentiment unit
 	public void addSentimentUnitList(ArrayList<SentimentUnit> suList){
 		this.sentimentList.addAll(suList);
-		return;
 	}
 	
 	public void ensemble(){
