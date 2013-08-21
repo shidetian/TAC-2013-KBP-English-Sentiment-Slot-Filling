c48946c903d6e7d507c0ddc0437a7d84aeab943d
LingjiaDeng <lingjiadeng@gmail.com>
2013-08-21 15:58:33 -0400

    Update MatchQuery.java

--- TAC SSF/src/MatchQuery.java
+++ TAC SSF/src/MatchQuery.java
@@ -2,20 +2,22 @@
 // the particular attribute of query is not determined?
 
 public class MatchQuery{
-	String filler;
+	public Response response;
 	
-	public MatchQuery(SentimentUnit su, Query query){
+	public MatchQuery(ArrayList<SentimentUnit> suList, Query query){
 		
-		// the following two cases are cases
-		// which the sentiment unit is in accordance with query
-		// that means 
-		// 1) when the query type is "pos/neg-from", the query is the target and the filler is the holder
-		if (query.sent.toString.contains("from") && query.entity.equals(su.targetSpan)){
-			filler = su.holderSpan;
-		}
-		// 2) when the query type is "pos/neg-towards", the query is the holder and the filler is the target
-		else if (query.sent.toString.contains("towards") && query.entity.equals(su.holderSpan)){
-			filler = su.targetSpan;
+		for (su: suList){
+			// the following two cases are cases
+			// which the sentiment unit is in accordance with query
+			// that means 
+			// 1) when the query type is "pos/neg-from", the query is the target and the filler is the holder
+			if (query.sent.toString.contains("from") && query.entity.equals(su.targetSpan)){
+				response = new Response(query.id, su.polarity, su.team, "", su.holderSpan, su.holderOffsets, su.targetOffsets, "", su.confidenceScore);
+			}
+			// 2) when the query type is "pos/neg-towards", the query is the holder and the filler is the target
+			else if (query.sent.toString.contains("towards") && query.entity.equals(su.holderSpan)){
+				response = new Response(query.id, su.polarity, su.team, "", su.targetSpan, su.targetOffsets, su.holderOffsets, "", su.confidenceScore);
+			}
 		}
 		
 	}
