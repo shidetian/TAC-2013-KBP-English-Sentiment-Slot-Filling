5fdb6c1ff649c3ce3ed8d331aa8ecfd5f3e8c93a
LingjiaDeng <lingjiadeng@gmail.com>
2013-08-22 23:11:35 -0400

    Update SentimentUnit.java

--- TAC SSF/src/SentimentUnit.java
+++ TAC SSF/src/SentimentUnit.java
@@ -6,19 +6,30 @@ public class SentimentUnit{
 	// we can add more public attributes, if needed
 	public String docID;  // document ID
 	public String sentenceSpan;  // span of the sentence containing this opinion
+	public String sentenceOffsets;
 	public String opinSpan;  // span of the opinion expression
+	public String opinOffsets;
 	public String holderSpan;  // span of the holder
+	public String holderOffsets
 	public String targetSpan;  // span of the target
+	public String targetOffsets;
 	public String polarity;    // polarity, pos or neg
 	public double confidenceScore;  // confidence score of the extracted opinion
 	
-	public SentimentUnit(String docID, String sentenceSpan, String opinSpan, String holderSpan, 
-			String targetSpan, String polarity, double confidenceScore){
+	public SentimentUnit(String docID, String sentenceSpan, String sentenceOffsets, 
+	                String opinSpan, String opinOffsets,
+	                String holderSpan, String holderOffsets,
+			String targetSpan, String, targetOffsets,
+			String polarity, double confidenceScore){
 		this.docID = docID;
 		this.sentenceSpan = sentenceSpan;
+		this.sentenceOffsets = sentenceOffsets;
 		this.opinSpan = opinSpan;
+		this.opinOffsets = opinOffsets;
 		this.holderSpan = holderSpan;
+		this.holderOffsets = holderOffsets;
 		this.targetSpan = targetSpan;
+		this.targetOffsets = targetOffsets;
 		this.polarity = polarity;
 		this.confidenceScore = confidenceScore; 
 	}
