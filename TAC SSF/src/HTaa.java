9bd09bb336caccd659a15b93eb7d5a4f80887c6e
LingjiaDeng <lingjiadeng@gmail.com>
2013-08-23 12:01:35 -0400

    Update HTaa.java

--- TAC SSF/src/HTaa.java
+++ TAC SSF/src/HTaa.java
@@ -41,3 +41,90 @@ public class HTaa {
                 targetHash.put("", -1);
 
                 p.getDependencyString(sentence);
+                                extractHolder();
+                extractTarget();
+                System.out.println(holderCandidates.length);
+                System.out.println(targetCandidates.length);
+                System.out.println(holderHash.size());
+                System.out.println(holderHash.containsKey("bb"));
+                rank();
+        }
+
+        private void extractHolder(){
+                for (int indexOpinWord=0;indexOpinWord<opinWords.size();indexOpinWord++){
+                        String candidate = null;
+                        int indexDependency =0;
+                        System.out.println(opinWords.size());
+                        System.out.println(opinWords.get(indexOpinWord));
+                        System.out.println(holderDependency[indexDependency]);
+                        while (candidate == null && indexDependency<holderDependency.length){
+                                candidate = p.getTheOtherWord(opinWords.get(indexOpinWord),holderDependency[indexDependency],"dep");
+                                indexDependency++;
+                        }
+                        holderCandidates[indexOpinWord] = candidate;
+                }
+                return;
+        }
+
+        private void extractTarget(){
+                for (int indexOpinWord=0;indexOpinWord<opinWords.size();indexOpinWord++){
+                        String candidate = null;
+                        int indexDependency =0;
+                        while (candidate == null && indexDependency<targetDependency.length){
+                                candidate = p.getTheOtherWord(opinWords.get(indexOpinWord),targetDependency[indexDependency],"gov");
+                                indexDependency++;
+                        }
+                        targetCandidates[indexOpinWord] = candidate;
+                }
+                return;
+        }
+
+        private void rank(){
+                for (String holder: holderCandidates){
+                        System.out.println("~~~"+holder);
+                        if (holder == null)
+                                continue;
+                        if (holderHash.containsKey(holder)){
+                                System.out.println("@##");
+                                int value = holderHash.get(holder);
+                                holderHash.put(holder, value+1);
+                        }
+                        else
+                                holderHash.put(holder, 1);
+                }
+
+                //Transfer as List and sort it
+                ArrayList<Map.Entry<String, Integer>> l = new ArrayList<Entry<String, Integer>>(holderHash.entrySet());
+                Collections.sort(l, new Comparator<Map.Entry<String, Integer>>(){
+
+                        public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                                return o2.getValue().compareTo(o1.getValue());
+                        }});
+
+                holderSpan = l.get(0).getKey();
+
+                for (String target: targetCandidates){
+                        if (target == null)
+                                continue;
+                        if (targetHash.containsKey(target)){
+                                int value = targetHash.get(target);
+                                targetHash.put(target, value+1);
+                        }
+                        else
+                                targetHash.put(target, 1);
+                }
+
+                //Transfer as List and sort it
+                l = new ArrayList<Entry<String, Integer>>(targetHash.entrySet());
+                Collections.sort(l, new Comparator<Map.Entry<String, Integer>>(){
+
+                        public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                                return o2.getValue().compareTo(o1.getValue());
+                        }});
+
+                targetSpan = l.get(0).getKey();
+
+        }
+}
+
+
