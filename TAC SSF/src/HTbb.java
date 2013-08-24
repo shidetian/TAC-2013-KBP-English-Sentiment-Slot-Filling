a4c1d93e09fdb98bf4c19ca9049ba82078c88a3b
LingjiaDeng <lingjiadeng@gmail.com>
2013-08-24 19:12:39 -0400

    Update HTbb.java

--- TAC SSF/src/HTbb.java
+++ TAC SSF/src/HTbb.java
@@ -3,126 +3,158 @@ package Parser;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.Map;
 import java.util.Map.Entry;
 
-//import HTParser.Parser;
+public class HTbb {
+	public ArrayList<String> opinWords;
+	public String sentence;
+	public String holderSpan;
+	public String targetSpan;
+	public ArrayList<String> entities;
+	public String printing;
+	public HashMap<String, String> results;
+	
+	
+	private HTParser p;
+	private Hashtable<String,Integer> holderCandidatesLength;
+	private Hashtable<String,Integer> targetCandidatesLength;
+	private Hashtable<String,Integer> holderCandidatesTimes;
+	private Hashtable<String,Integer> targetCandidatesTimes;
+	private Hashtable<String,Float> holderHash;
+	private Hashtable<String,Float> targetHash;
 
-//import opinUnit.SentimentUnit;
-//import opinWords.lookUpOpinLeixcon;
-//import opinWords.lookUpOpinLeixcon.OpinWord;
+	public HTbb(){
+		
+		this.p = new HTParser();
+		this.holderCandidatesLength = new Hashtable<String,Integer>();
+		this.targetCandidatesLength = new Hashtable<String,Integer>();
+		this.holderCandidatesTimes = new Hashtable<String,Integer>();
+		this.targetCandidatesTimes = new Hashtable<String,Integer>();
+		this.holderHash = new Hashtable<String,Float>();
+		this.targetHash = new Hashtable<String,Float>();
+		
+		
+	}
+	
+	public HashMap<String, String> process(String s, ArrayList<String> opinWords, ArrayList<String> entities){
+		HashMap<String, String> results = new HashMap<String, String>();
+		opinWords = opinWords;
+		sentence = s;
+		entities = entities;
+		
+		p.getDependencyString(sentence);
+		extractHT();
+		rank();
+		
+		printing = sentence + "\t" + holderSpan + "\t" + targetSpan;
+		results.put("0_"+String.valueOf(s.length()-1), printing);
+		
+		return results;
+		
+	}
+	
+	private void extractHT(){
+		for (int indexOpinWord=0;indexOpinWord<opinWords.size();indexOpinWord++){
+			for (int indexEntity=0;indexEntity<entities.size();indexEntity++){
+				String trace = p.getTheRelationBetween(opinWords.get(indexOpinWord), entities.get(indexEntity));
+				Boolean holderFlag = traceJudgeHolder(trace);
+				Boolean targetFlag = traceJudgeTarget(trace);
+				
+				if (holderFlag && !targetFlag){
+					if (holderCandidatesLength.contains(entities.get(indexEntity))){
+						int value = holderCandidatesLength.get(entities.get(indexEntity)) + trace.split("-").length-1;
+						holderCandidatesLength.put(entities.get(indexEntity),value);
+						int times = holderCandidatesTimes.get(entities.get(indexEntity)) + 1;
+						holderCandidatesTimes.put(entities.get(indexEntity), times);
+					}
+					else{
+						holderCandidatesLength.put(entities.get(indexEntity),trace.split("-").length-1);
+						holderCandidatesTimes.put(entities.get(indexEntity), 1);
+					}
+				}
+				else if (!holderFlag && targetFlag){
+					if (targetCandidatesLength.contains(entities.get(indexEntity))){
+						int value = targetCandidatesLength.get(entities.get(indexEntity)) + trace.split("-").length-1;
+						targetCandidatesLength.put(entities.get(indexEntity),value);
+						int times = targetCandidatesTimes.get(entities.get(indexEntity)) + 1;
+						targetCandidatesTimes.put(entities.get(indexEntity), times);
+					}
+					else{
+						targetCandidatesLength.put(entities.get(indexEntity),trace.split("-").length-1);
+						targetCandidatesTimes.put(entities.get(indexEntity), 1);
+					}
+				}
+			}
+		}
+		return;
+	}
+	
+	private Boolean traceJudgeHolder(String trace){
+		if (!trace.contains("subj"))
+			return false;
+		
+		String[] relations = trace.split("-");
+		String lastRelation = relations[relations.length-1];
+		if (lastRelation.contains("nn") || lastRelation.contains("subj") || lastRelation.contains("poss") ){
+			return true;
+		}
+		
+		
+		return false;
+	}
+	
+	private Boolean traceJudgeTarget(String trace){
+		if (!trace.contains("obj"))
+			return false;
+		
+		String[] relations = trace.split("-");
+		String lastRelation = relations[relations.length-1];
+		if (lastRelation.contains("nn") || lastRelation.contains("subjpass") || lastRelation.contains("obj") ){
+			return true;
+		}
+		return false;
+	}
+	
+	private void rank(){
+		for (String holder: holderCandidatesLength.keySet()){
+			if (holder == null)
+				continue;
+			holderHash.put(holder, holderCandidatesLength.get(holder)/(float)holderCandidatesTimes.get(holder) );
+		}
 
+		//Sort
+		ArrayList<Map.Entry<String, Float>> l = new ArrayList<Entry<String, Float>>(holderHash.entrySet());
+		Collections.sort(l, new Comparator<Map.Entry<String, Float>>(){
 
-public class HTaa {
+			public int compare(Map.Entry<String, Float> o1, Map.Entry<String, Float> o2) {
+				return o2.getValue().compareTo(o1.getValue());
+			}});
+		
+		//output
+		holderSpan = l.get(0).getKey();
+		
+		/////////////////////////////////////////////////
 
-        public ArrayList<String> opinWords;
-        public String sentence;
-        public String holderSpan;
-        public String targetSpan;
-        private HTParser p;
-        private String[] holderDependency = {"subj","comp"}; // this should have an order
-        private String[] targetDependency = {"amod","obj","subj"};
-        private String[] holderCandidates;
-        private String[] targetCandidates;
-        private Hashtable<String,Integer> holderHash;
-        private Hashtable<String,Integer> targetHash;
+		for (String target: targetCandidatesLength.keySet()){
+			if (target == null)
+				continue;
+			targetHash.put(target, targetCandidatesLength.get(target)/(float)targetCandidatesTimes.get(target) );
+		}
 
-        public HTaa(ArrayList<String> opinWords, String s){
-                this.opinWords = opinWords;
-                sentence = s;
-                p = new HTParser();
+		//Sort
+		l = new ArrayList<Entry<String, Float>>(targetHash.entrySet());
+		Collections.sort(l, new Comparator<Map.Entry<String, Float>>(){
 
-                holderCandidates = new String[opinWords.size()];
-                targetCandidates = new String[opinWords.size()];
-                holderHash = new Hashtable<String,Integer>();
-                holderHash.put("", -1);
-                targetHash = new Hashtable<String,Integer>();
-                targetHash.put("", -1);
+			public int compare(Map.Entry<String, Float> o1, Map.Entry<String, Float> o2) {
+				return o2.getValue().compareTo(o1.getValue());
+			}});
+		
+		//output
+		targetSpan = l.get(0).getKey();
 
-                p.getDependencyString(sentence);
-                                extractHolder();
-                extractTarget();
-                System.out.println(holderCandidates.length);
-                System.out.println(targetCandidates.length);
-                System.out.println(holderHash.size());
-                System.out.println(holderHash.containsKey("bb"));
-                rank();
-        }
+	}
 
-        private void extractHolder(){
-                for (int indexOpinWord=0;indexOpinWord<opinWords.size();indexOpinWord++){
-                        String candidate = null;
-                        int indexDependency =0;
-                        System.out.println(opinWords.size());
-                        System.out.println(opinWords.get(indexOpinWord));
-                        System.out.println(holderDependency[indexDependency]);
-                        while (candidate == null && indexDependency<holderDependency.length){
-                                candidate = p.getTheOtherWord(opinWords.get(indexOpinWord),holderDependency[indexDependency],"dep");
-                                indexDependency++;
-                        }
-                        holderCandidates[indexOpinWord] = candidate;
-                }
-                return;
-        }
-
-        private void extractTarget(){
-                for (int indexOpinWord=0;indexOpinWord<opinWords.size();indexOpinWord++){
-                        String candidate = null;
-                        int indexDependency =0;
-                        while (candidate == null && indexDependency<targetDependency.length){
-                                candidate = p.getTheOtherWord(opinWords.get(indexOpinWord),targetDependency[indexDependency],"gov");
-                                indexDependency++;
-                        }
-                        targetCandidates[indexOpinWord] = candidate;
-                }
-                return;
-        }
-
-        private void rank(){
-                for (String holder: holderCandidates){
-                        System.out.println("~~~"+holder);
-                        if (holder == null)
-                                continue;
-                        if (holderHash.containsKey(holder)){
-                                System.out.println("@##");
-                                int value = holderHash.get(holder);
-                                holderHash.put(holder, value+1);
-                        }
-                        else
-                                holderHash.put(holder, 1);
-                }
-
-                //Transfer as List and sort it
-                ArrayList<Map.Entry<String, Integer>> l = new ArrayList<Entry<String, Integer>>(holderHash.entrySet());
-                Collections.sort(l, new Comparator<Map.Entry<String, Integer>>(){
-
-                        public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
-                                return o2.getValue().compareTo(o1.getValue());
-                        }});
-
-                holderSpan = l.get(0).getKey();
-
-                for (String target: targetCandidates){
-                        if (target == null)
-                                continue;
-                        if (targetHash.containsKey(target)){
-                                int value = targetHash.get(target);
-                                targetHash.put(target, value+1);
-                        }
-                        else
-                                targetHash.put(target, 1);
-                }
-
-                //Transfer as List and sort it
-                l = new ArrayList<Entry<String, Integer>>(targetHash.entrySet());
-                Collections.sort(l, new Comparator<Map.Entry<String, Integer>>(){
-
-                        public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
-                                return o2.getValue().compareTo(o1.getValue());
-                        }});
-
-                targetSpan = l.get(0).getKey();
-
-        }
 }
