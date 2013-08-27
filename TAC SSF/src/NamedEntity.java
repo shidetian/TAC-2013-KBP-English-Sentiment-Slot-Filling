6d24066afafc9550a458f00eb40d525939a9c8c4
yoonjung <choiyj35@gmail.com>
2013-08-27 14:51:53 -0400

    Create NamedEntity.java

---
+++ TAC SSF/src/NamedEntity.java
@@ -0,0 +1,14 @@
+
+public class NamedEntity {
+	String entityid;
+	String entity;
+	int beg;
+	int end;
+	
+	public NamedEntity(String id, String entity, int beg, int end){
+		this.entityid = id;
+		this.entity = entity;
+		this.beg = beg;
+		this.end = end;
+	}
+}
