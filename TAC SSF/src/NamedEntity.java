074b6c8ae81026e0806c1c050fa81cc9dc162acf
yoonjung <choiyj35@gmail.com>
2013-08-28 15:28:22 -0400

    Update NamedEntity.java

--- TAC SSF/src/NamedEntity.java
+++ TAC SSF/src/NamedEntity.java
@@ -1,13 +1,17 @@
+import java.util.*;
+
 public class NamedEntity {
-	public String entityid;
-	public String entity;
-	public int beg;
-	public int end;
+	String entityid;
+	String entity;
+	int beg;
+	int end;
+	HashMap<String, Double> resolutions;
 	
-	public NamedEntity(String id, String entity, int beg, int end){
+	public NamedEntity(String id, String entity, int beg, int end, HashMap<String, Double> resolutions){
 		this.entityid = id;
 		this.entity = entity;
 		this.beg = beg;
 		this.end = end;
+		this.resolutions = resolutions;
 	}
 }
