f9dd1bec910b17761b9c257d4f9a47e15df8bfad
LingjiaDeng <lingjiadeng@gmail.com>
2013-08-28 16:08:15 -0400

    compare by id in knowledge base

--- TAC SSF/src/MatchQuery.java
+++ TAC SSF/src/MatchQuery.java
@@ -1,4 +1,9 @@
+package NE;
+
 import java.util.ArrayList;
+import java.util.List;
+
+import opinUnit.SentimentUnit;
 
 // analyze the sentiment unit, whether it is consistent with the query
 // the particular attribute of query is not determined?
@@ -9,15 +14,50 @@ public class MatchQuery{
 	public MatchQuery(ArrayList<SentimentUnit> suList, Query query){
 		
 		for (SentimentUnit su: suList){
+			int[] holderOffsets = {Integer.parseInt(su.holderOffsets.split("-")[0]), Integer.parseInt(su.holderOffsets.split("-")[1])};
+			int[] targetOffsets = {Integer.parseInt(su.targetOffsets.split("-")[0]), Integer.parseInt(su.targetOffsets.split("-")[1])};
+			
+			NEReader NE1 = new NEReader(null);
+			NE1.parseNEs(su.docID);
+			List<NamedEntity> holders = NE1.getNEs(holderOffsets[0], holderOffsets[1]);
+			List<NamedEntity> targets = NE1.getNEs(targetOffsets[0], targetOffsets[1]);
+			
+			Boolean holderFlag = false;
+			Boolean targetFlag = false;
+			
+			// holder
+			for (NamedEntity holder: holders){
+				if (query.nodeId.charAt(0)=='E'){
+					if (holder.resolutions.containsKey(query.nodeId))
+						holderFlag = true;
+				} // if in KB
+				else{
+					if (query.entity.equals(holder.entity))
+						holderFlag = true;
+				}
+			}
+			
+			// target
+			for (NamedEntity target: targets){
+				if (query.nodeId.charAt(0)=='E'){
+					if (target.resolutions.containsKey(query.nodeId))
+						targetFlag = true;
+				} // if in KB
+				else{
+					if (query.entity.equals(target.entity))
+						targetFlag = true;
+				}
+			}
+			
 			// the following two cases are cases
 			// which the sentiment unit is in accordance with query
 			// that means 
 			// 1) when the query type is "pos/neg-from", the query is the target and the filler is the holder
-			if (query.sent.toString().contains("from") && query.entity.equals(su.targetSpan)){
+			if (query.sent.toString().contains("from") && targetFlag){
 				response = new Response(query.id, su.polarity, su.team, "", su.holderSpan, su.holderOffsets, su.targetOffsets, "", su.confidenceScore);
 			}
 			// 2) when the query type is "pos/neg-towards", the query is the holder and the filler is the target
-			else if (query.sent.toString().contains("towards") && query.entity.equals(su.holderSpan)){
+			else if (query.sent.toString().contains("towards") && holderFlag){
 				response = new Response(query.id, su.polarity, su.team, "", su.targetSpan, su.targetOffsets, su.holderOffsets, "", su.confidenceScore);
 			}
 		}
