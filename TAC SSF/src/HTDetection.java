78d9d82ba7dd860c19a90cc574fb0e4e1ea168e7
yoonjung <choiyj35@gmail.com>
2013-08-28 15:04:53 -0400

    Update HTDetection.java

--- TAC SSF/src/HTDetection.java
+++ TAC SSF/src/HTDetection.java
@@ -1,96 +1,115 @@
-import java.io.StringReader;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-import edu.stanford.nlp.ling.CoreLabel;
-import edu.stanford.nlp.parser.lexparser.LexicalizedParser;
-import edu.stanford.nlp.process.CoreLabelTokenFactory;
-import edu.stanford.nlp.process.PTBTokenizer;
-import edu.stanford.nlp.process.TokenizerFactory;
-import edu.stanford.nlp.trees.GrammaticalStructure;
-import edu.stanford.nlp.trees.GrammaticalStructureFactory;
-import edu.stanford.nlp.trees.PennTreebankLanguagePack;
-import edu.stanford.nlp.trees.Tree;
-import edu.stanford.nlp.trees.TreebankLanguagePack;
 import edu.stanford.nlp.trees.TypedDependency;
 
 
 public class HTDetection {
-	static LexicalizedParser lp;
+	//static LexicalizedParser lp;
 	
 	public HTDetection(){
-		lp = LexicalizedParser.loadModel("edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz");
+	//	lp = LexicalizedParser.loadModel("edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz");
 	}
 	
-	public HashMap<String, String> process(Sentence sent, HashSet<String> terms){
+	public static HashMap<String, String> process(Sentence sent, String dep, HashSet<String> terms, 
+			List<NamedEntity> entities, String author, String aidx){
+		
 		HashMap<String, String> ht = new HashMap<String, String>();
 		HashMap<String, String> opinterms = new HashMap<String, String>();
 		for(String term : terms)
 			opinterms.put(term, term);
 		
-		List<TypedDependency> tdl = parsing(sent.sent);
-		HashMap<String, String> Scandidates = findCandidates(tdl, opinterms, "subj");
-		HashMap<String, String> Ocandidates = findCandidates(tdl, opinterms, "obj");
-							
-		Set<String> Skey = Scandidates.keySet();
-		Iterator<String> Siter = Skey.iterator();
-		while(Siter.hasNext()){
-			String subj = Siter.next();
-			String Sopin = Scandidates.get(subj);
-			subj = subj.substring(0, subj.indexOf("-"));
+		//System.out.println(dep);
+		
+		List<DependencyTriple> dtl = getDependencyTripleList(dep);
+		
+		HashMap<String, String> subjTmp = findCandidates(dtl, opinterms, "subj");
+		HashMap<String, String> objTmp = findCandidates(dtl, opinterms, "obj");
+		
+		//System.out.println("SubjTmp: " + subjTmp);
+		//System.out.println("ObjTmp: " + objTmp);
+		
+		HashMap<NamedEntity, String> subjCands = checkNEs(subjTmp, entities);
+		HashMap<NamedEntity, String> objCands = checkNEs(objTmp, entities);
+		
+		//System.out.println("SubjCands: ");
+		//System.out.println("ObjCnads: " + objCands);
+			
+		Set<NamedEntity> subjKey = subjCands.keySet();
+		Iterator<NamedEntity> subjIter = subjKey.iterator();
+		while(subjIter.hasNext()){
+			NamedEntity subj = subjIter.next();
+			//System.out.println("  " + subj.entity);
+			String subjopinterm = subjCands.get(subj);
 			boolean check = false;
 			
-			if(Ocandidates.size() > 0){
-				Set<String> Okey = Ocandidates.keySet();
-				Iterator<String> Oiter = Okey.iterator();
-				while(Oiter.hasNext()){
-					String obj = Oiter.next();
-					String Oopin = Ocandidates.get(obj);
-					obj = obj.substring(0, obj.indexOf("-"));
+			if(objCands.size() > 0){
+				Set<NamedEntity> objKey = objCands.keySet();
+				Iterator<NamedEntity> objIter = objKey.iterator();
+				while(objIter.hasNext()){
+					NamedEntity obj = objIter.next();
+					String objopinterm = objCands.get(obj);
 					
-					if(Sopin.compareTo(Oopin) == 0){
-						String opin = Oopin.concat("\t").
-								concat(Integer.toString(sent.sent.indexOf(Oopin)+sent.beg).concat("-").
-								concat(Integer.toString(sent.sent.indexOf(Oopin)+sent.beg+Oopin.length())));
-						String holder = subj.concat("\t").
-								concat(Integer.toString(sent.sent.indexOf(subj)+sent.beg).concat("-").
-								concat(Integer.toString(sent.sent.indexOf(subj)+sent.beg+subj.length())));;
-						String target = obj.concat("\t").
-								concat(Integer.toString(sent.sent.indexOf(obj)+sent.beg).concat("-").
-								concat(Integer.toString(sent.sent.indexOf(obj)+sent.beg+obj.length())));;
-						ht.put(Oopin, opin.concat("\t").concat(holder).concat("\t").concat(target));
-						//String p = polarity.get(Oopin); 
-						//senti.add(new SentimentUnit(sSpan, oSpan, hSpan, tSpan, p, 0));
+					if(objopinterm.compareTo(subjopinterm) == 0){
 						check = true;
+						String opin = objopinterm.concat("\t").
+								concat(Integer.toString(sent.sent.indexOf(objopinterm)+sent.beg).concat("-").
+								concat(Integer.toString(sent.sent.indexOf(objopinterm)+sent.beg+objopinterm.length())));
+						String holder = subj.entity.concat("\t").
+								concat(Integer.toString(subj.beg).concat("-").
+								concat(Integer.toString(subj.end)));
+						String target = obj.entity.concat("\t").
+								concat(Integer.toString(obj.beg).concat("-").
+								concat(Integer.toString(obj.end)));
+						ht.put(objopinterm, opin.concat("\t").concat(holder).concat("\t").concat(target));
 					}
-					
 				}
 			}
 			
-			if(!check){
-				String opin = Sopin.concat("\t").
-						concat(Integer.toString(sent.sent.indexOf(Sopin)+sent.beg).concat("-").
-						concat(Integer.toString(sent.sent.indexOf(Sopin)+sent.beg+Sopin.length())));
-				String holder = "\t";
-				String target = subj.concat("\t").
-						concat(Integer.toString(sent.sent.indexOf(subj)+sent.beg).concat("-").
-						concat(Integer.toString(sent.sent.indexOf(subj)+sent.beg+subj.length())));;
-				ht.put(Sopin, opin.concat("\t").concat(holder).concat("\t").concat(target));
-				//System.out.println("Sopin : " + Sopin);
-				//String p = polarity.get(Sopin); 
-				//senti.add(new SentimentUnit(sSpan, oSpan, hSpan, tSpan, p, 0));
+			if((!check) && (author.length() >= 1)){
+				String opin = subjopinterm.concat("\t").
+						concat(Integer.toString(sent.sent.indexOf(subjopinterm)+sent.beg).concat("-").
+						concat(Integer.toString(sent.sent.indexOf(subjopinterm)+sent.beg+subjopinterm.length())));
+				String holder = author.concat("\t").concat(aidx);
+				String target = subj.entity.concat("\t").
+						concat(Integer.toString(subj.beg).concat("-").
+						concat(Integer.toString(subj.end)));
+				ht.put(subjopinterm, opin.concat("\t").concat(holder).concat("\t").concat(target));
 			}
-		}	
+		}
+
 		return ht;
 	}
 	
-	public static HashMap<String, String> findCandidates(List<TypedDependency> tdl, HashMap<String, String> terms, String type){
+
+	public static HashMap<NamedEntity, String> checkNEs(HashMap<String, String> cands, List<NamedEntity> entities){
+		HashMap<NamedEntity, String> candidates = new HashMap<NamedEntity, String>();
+		
+		Set<String> keyset = cands.keySet();
+		Iterator<String> iter = keyset.iterator();
+		while(iter.hasNext()){
+			String term = iter.next();
+			
+			for(NamedEntity entity : entities){
+				//System.out.println(entity.entity + " , " + term);
+				String[] toks = entity.entity.split(" ");
+				for(String tok : toks){
+					if((tok.compareTo(term) == 0) && (tok.length() > 1)){
+						candidates.put(entity, cands.get(term));
+						break;
+					}
+				}
+			}
+		}
+		
+		return candidates;
+	}
+	
+	public static HashMap<String, String> findCandidates(List<DependencyTriple> dtl, HashMap<String, String> terms, String rel){
 		HashMap<String, String> candidates = new HashMap<String, String>();
 		int cnt = 0;
-		
+		//System.out.println("terms : " + terms);
 		while((cnt++) < 5){
 			HashMap<String, String> newterms = new HashMap<String, String>();
 			Set<String> keyset = terms.keySet();
@@ -99,20 +118,21 @@ public class HTDetection {
 			while(iter.hasNext()){
 				String term = iter.next();
 				String orig = terms.get(term);
-				HashMap<String, String> parents = getParents(term, orig, tdl);
-				HashMap<String, String> children = getChildren(term, orig, tdl);
+				HashMap<String, String> parents = getParents(term, orig, dtl);
+				HashMap<String, String> children = getChildren(term, orig, dtl);
 				//System.out.println("Parent: " + parents);
 				//System.out.println("Children: " + children);
-				candidates.putAll(checkType(type, parents, tdl));
-				candidates.putAll(checkType(type, children, tdl));
+				candidates.putAll(checkRelation(rel, parents, dtl));
+				candidates.putAll(checkRelation(rel, children, dtl));
 				newterms.putAll(parents);
 				newterms.putAll(children);
 			}
 			
-			//System.out.println("new terms : " + newterms);
-		
-			if(candidates.size() > 0)
+			
+			if(candidates.size() > 0){
+				//System.out.println("Candidates : " + candidates);
 				break;
+			}
 			
 			if(newterms.size() <= 0)
 				break;
@@ -128,64 +148,84 @@ public class HTDetection {
 	}
 	
 	
-	public static HashMap<String, String> checkType(String type, HashMap<String, String> list, List<TypedDependency> tdl){
+	public static HashMap<String, String> checkRelation(String rel, HashMap<String, String> list, List<DependencyTriple> dtl){
 		HashMap<String, String> keyset = new HashMap<String, String>();
 		
 		Set<String> key = list.keySet();
 		Iterator<String> iter = key.iterator();
 		while(iter.hasNext()){
 			String term = iter.next();
-			for(TypedDependency td : tdl){
-				String[] toks = td.toString().split("[(), ]+");
-				if(toks[2].contains(term) && toks[0].contains(type)){
+			for(DependencyTriple dt : dtl){
+				if(dt.dep.contains(term) && dt.relation.contains(rel)){
 					keyset.put(term, list.get(term));
+					//keyset.putAll(getChildren(term, list.get(term), dtl));
 				}
 			}
 		}
 		
-		//System.out.println("keyset : " + keyset);
 		return keyset;
 	}
 	
-	public static HashMap<String, String> getParents(String term, String orig, List<TypedDependency> tdl){
+	public static HashMap<String, String> getParents(String term, String orig, List<DependencyTriple> dtl){
 		HashMap<String, String> parents = new HashMap<String, String>();
 		
-		for(TypedDependency td : tdl){
-			String[] toks = td.toString().split("[()-, ]+");
-			if(toks[2].contains(term)){
-				parents.put(toks[1], orig);
+		for(DependencyTriple dt : dtl){
+			if(dt.dep.contains(term)){
+				parents.put(dt.gov, orig);
 			}
 		}
 		
 		return parents;
 	}
 
-	public static HashMap<String, String> getChildren(String term, String orig, List<TypedDependency> tdl){
+	public static HashMap<String, String> getChildren(String term, String orig, List<DependencyTriple> dtl){
 		HashMap<String, String> children = new HashMap<String, String>();
 		
-		for(TypedDependency td : tdl){
-			String[] toks = td.toString().split("[()-, ]+");
-			if(toks[1].contains(term)){
-				children.put(toks[2], orig);
+		for(DependencyTriple dt : dtl){
+			if(dt.gov.contains(term)){
+				children.put(dt.dep, orig);
 			}
 		}
 		
 		return children;
 	}
+	
+	public static List<DependencyTriple> getDependencyTripleList(String dep){
+		List<DependencyTriple> dependencyTripleList = new ArrayList<DependencyTriple>();
+		
+		String tempDep = dep.replace("), ", "),~");
+		String[] tripleList = tempDep.split(",~");
 
-	public static List<TypedDependency> parsing(String sent){
-		
-		TokenizerFactory<CoreLabel> tokenizerFactory = PTBTokenizer.factory(new CoreLabelTokenFactory(), "");
-		List<CoreLabel> rawWords = tokenizerFactory.getTokenizer(new StringReader(sent)).tokenize();
-		Tree parse = lp.apply(rawWords);
+		for (int i=0;i<tripleList.length;i++){
+			String dependencyTripleString = tripleList[i];
 
-		TreebankLanguagePack tlp = new PennTreebankLanguagePack();
-		GrammaticalStructureFactory gsf = tlp.grammaticalStructureFactory();
-		GrammaticalStructure gs = gsf.newGrammaticalStructure(parse);
-		List<TypedDependency> tdl = gs.typedDependenciesCCprocessed();
+			Pattern triplePattern = Pattern.compile("([a-z_]+)\\((.*?)\\-[0-9]+, (.*?)\\-[0-9]+");
+			Matcher tripleMatcher = triplePattern.matcher(dependencyTripleString);
+			while (tripleMatcher.find()){
+				String dependencyRel = tripleMatcher.group(1);
+				String dependencyGov = tripleMatcher.group(2);
+				String dependencyDep = tripleMatcher.group(3);
+
+				dependencyTripleList.add(new DependencyTriple(dependencyRel, dependencyGov, dependencyDep));
+				
+				//System.out.println(dependencyRel + " , " + dependencyGov + " , " + dependencyDep);
+			}
+		}
 		
-		//System.out.println("  " + tdl.toString());
-		
-		return tdl;
+		//System.out.println(toks);
+		return dependencyTripleList;
+	}
+	
+}
+
+class DependencyTriple{
+	public String gov;
+	public String dep;
+	public String relation;
+	
+	public DependencyTriple(String relation, String gov, String dep){
+		this.relation = relation;
+		this.gov = gov;
+		this.dep = dep;
 	}
 }
